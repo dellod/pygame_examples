9f5aa8f19dddb4bdcae7715664b165f64791bbda
Daryl Dang <daryl.dang4@gmail.com>
2022-01-06 15:36:54 -0700

    Add example 1 to session 1
    Variable checkpoint

---
+++ Session1/example1_variable_checkpoint/example1_variable_checkpoint.py
@@ -0,0 +1,17 @@
+# !/usr/bin/env python3
+# @file broken_code.py
+# SCRP: Session1 - Variable Checkpoint
+# Daryl Dang
+
+# Initialize Variables
+integer_variable = 1
+float_variable = 3.14  # this is PI!
+string_variable = "Di$c0v3r PrOgr@m!"
+boolean_variable = True
+
+# Print out variables
+print("integer_variable = {} and it's type is {}".format(integer_variable, type(integer_variable)))
+print("float_variable = {} and it's type is {}".format(float_variable, type(float_variable)))
+print("string_variable = {} and it's type is {}".format(string_variable, type(string_variable)))
+print("boolean_variable = {} and it's type is {}".format(boolean_variable, type(boolean_variable)))
+
