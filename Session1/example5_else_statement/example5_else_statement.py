6b86f77aa9de1c56ff18c991fdaf27f1b2e010d5
Daryl Dang <daryl.dang4@gmail.com>
2022-01-13 12:46:36 -0700

    Add example 5 to session 1

---
+++ Session1/example5_else_statement/example5_else_statement.py
@@ -0,0 +1,16 @@
+# !/usr/bin/env python3
+# @file example5_else_statement.py
+# SCRP: Session 1 - Example 5: Else Statement
+# Daryl Dang
+
+"""
+This program demonstrates the usage of an else statement.
+"""
+
+a = -300
+b = -400
+
+if b > a:
+    print("b is greater than a!")
+else:
+    print("a is greater than b!")
