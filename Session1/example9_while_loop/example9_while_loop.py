b989ee663a0f64ea39f40791b6d04c0d1c6d3215
Daryl Dang <daryl.dang4@gmail.com>
2022-01-15 17:57:25 -0700

    Add example 9 to session 1

---
+++ Session1/example9_while_loop/example9_while_loop.py
@@ -0,0 +1,20 @@
+# !/usr/bin/env python3
+# @file example9_while_loop.py
+# SCRP: Session 1 - Example 8: While Loop
+# Daryl Dang
+
+"""
+This program demonstrates a basic while loop. It also showcases what a bad while loop looks like.
+"""
+
+# Basic while loop
+i = 1         # Set up initial counter variable.
+while i < 4:  # Basic while loop condition with counter variable.
+    print(i)
+    i += 1    # Increment counter variable.
+
+# Bad while loop (uncomment to test) - why is this a bad while loop?
+# i = 1
+# while i < 4:
+#     print(i)
+#     i -= 1      # i = i - 1 # The same statement but this is shorthand.
