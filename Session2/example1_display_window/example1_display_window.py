2278e9d28637555d8d97005955591b40295296d7
Daryl <43867408+dellod@users.noreply.github.com>
2022-11-08 01:23:46 -0700

    [Session 2] Add example 1
    * Add example 10
    
    * Add initial example 1 code
    
    * [Session 2] Add example 1

---
+++ Session2/example1_display_window/example1_display_window.py
@@ -0,0 +1,24 @@
+# !/usr/bin/env python3
+# @file example1_display_window.py
+# SCRP: Example 1 - Display Window
+# Daryl Dang
+
+# 1. Need to import the PyGame library
+import pygame
+
+# 2. Initialize all the PyGame modules
+pygame.init()
+
+# 3. Create the display window
+display = pygame.display.set_mode((500, 500))  # 500 pixels by 500 pixels
+
+# 4. Create a "game" (infinite) loop
+running = True
+while running:
+    for event in pygame.event.get():
+        # Quit when the user presses the X at the top right
+        if event.type == pygame.QUIT:
+            running = False
+
+# 5. Close and clean up
+pygame.quit()
