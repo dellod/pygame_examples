051bf92ce20ac8bfbd76a7ee8899c977767d5658
dellod <daryl.dang4@gmail.com>
2022-11-13 19:22:25 -0700

    [Session 2] Fix example 1

--- Session2/example1_display_window/example1_display_window.py
+++ Session2/example1_display_window/example1_display_window.py
@@ -11,10 +11,7 @@ pygame.init()
 
 # 3. Create the display window
 display = pygame.display.set_mode((500, 500))  # 500 pixels by 500 pixels
-<<<<<<< HEAD
-=======
 pygame.display.set_caption("My Game") # Set the name of the window
->>>>>>> 5f909bad9b06152ab1efb5c4f1f0fab9e87953de
 
 # 4. Create a "game" (infinite) loop
 running = True
