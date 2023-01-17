a7cd696e6c20ae87b4a0d0728375a74a221aee1c
Daryl Dang <daryl.dang4@gmail.com>
2023-01-17 01:28:19 -0700

    [Session 3] Add example 8

---
+++ Session3/example8_basic_mouse_position_collision_detection/example8_basic_mouse_position_collision_detection.py
@@ -0,0 +1,53 @@
+# !/usr/bin/env python3
+# @file example8_basic_collision_detection.py
+# SCRP: Example 8 - Basic Mouse Position Collision Detection
+# Daryl Dang
+
+"""
+Example 8 - Basic Mouse Position Collision Detection
+----------------------------------------------------
+This example demonstrates basic collision detection with your mouse and drawn object on the screen
+using collidepoint().
+"""
+
+import pygame
+
+# GLOBALS
+SCREEN_WIDTH = 500
+SCREEN_HEIGHT = 500
+COLOUR = [255, 255, 255]
+CENTER = (250, 250)
+RADIUS = 100
+
+# Initialize pygame and display window (this isn't that crucial for this basic program)
+pygame.init()
+display = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Basic Mouse Position Collision Detection")
+
+# Draw circle first and store it in a variable so we can use it later in the loop
+circ = pygame.draw.circle(display, COLOUR, CENTER, RADIUS, 0)
+
+# Game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        # Quit when the user presses the X at the top right
+        if event.type == pygame.QUIT:
+            running = False
+
+    point = pygame.mouse.get_pos()
+    collide = circ.collidepoint(point)
+    if collide:
+        COLOUR = [255, 0, 0]
+    else:
+        COLOUR = [255, 255, 255]
+
+    # Draw circle again and update with new colour
+    display.fill(0)
+    pygame.draw.circle(display, COLOUR, CENTER, RADIUS, 0)
+
+    # Update the pygame window
+    pygame.display.update()
+
+# Close and clean up
+pygame.quit()
