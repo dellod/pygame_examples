404a69f6533bddc2430424f9946fb31b6ce84946
Daryl Dang <daryl.dang4@gmail.com>
2023-01-31 01:13:03 -0700

    [Session 7] Add example 1

---
+++ Session7/example1_basic_class/example1_basic_class.py
@@ -0,0 +1,16 @@
+# !/usr/bin/env python3
+# @file example1_basic_class.py
+# SCRP: Example 1 - Basic Class
+# Daryl Dang
+
+"""
+Example 1 - Basic Class
+-----------------------
+This example goes over how to create a very basic class and how to make an object out of it.
+"""
+
+class Animal:
+    name = "Clifford"
+
+dog = Animal()
+print(dog.name)
